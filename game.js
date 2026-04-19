96781a5e746e6d78d35fc472e58bc05cdcd1c957
MohammadAirout <98384576+Mohammaaddd@users.noreply.github.com>
2026-04-19 20:53:36 +0300

    add pacman he can move and eat

--- game.js
+++ game.js
@@ -11,12 +11,23 @@ let createRect = (x, y, width, height, color) => {
 
 //game variables
 let fps = 30;
+let pacman;
 let oneBlockSize = 20;
 let wallColor = "#342dca";
+let wallSpaceWidth = oneBlockSize / 1.5;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
+let foodColor = "#feb897";
+let score = 0;
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
 
 //creating a map 0 for the area outside the map
 //1 for the wall
-//2 for the floor
+//2 for the food
 let map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
@@ -50,11 +61,16 @@ let gameLoop = () => {
 
 function update() {
   //todo
+  pacman.moveProcess();
+  pacman.eat();
 }
 
 function draw() {
-  //todo
+  createRect(0, 0, canvas.width, canvas.height, "black");
   drawWalls();
+  drawFood();
+  drawScore();
+  pacman.draw();
 }
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
@@ -71,7 +87,101 @@ function drawWalls() {
           oneBlockSize,
           wallColor,
         );
+
+        if (j > 0 && map[i][j - 1] == 1) {
+          createRect(
+            j * oneBlockSize,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColor,
+          );
+        }
+
+        if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColor,
+          );
+        }
+
+        if (i > 0 && map[i - 1][j] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColor,
+          );
+        }
+
+        if (i < map.length - 1 && map[i + 1][j] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColor,
+          );
+        }
       }
     }
   }
 }
+
+function drawFood() {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] == 2) {
+        createRect(
+          j * oneBlockSize + oneBlockSize / 3,
+          i * oneBlockSize + oneBlockSize / 3,
+          oneBlockSize / 3,
+          oneBlockSize / 3,
+          foodColor,
+        );
+      }
+    }
+  }
+}
+
+function drawScore() {
+  canvasContext.font = "20px Emulogic";
+  canvasContext.fillStyle = "white";
+  canvasContext.fillText("Score: " + score, 0, oneBlockSize * (map.length + 1));
+}
+
+function createNewPacman() {
+  pacman = new Pacman(
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize / 5,
+  );
+}
+
+createNewPacman();
+gameLoop();
+
+window.addEventListener("keydown", (event) => {
+  let k = event.keyCode;
+  setTimeout(() => {
+    if (k == 37 || k == 65) {
+      // left arrow or a
+      pacman.nextDirection = DIRECTION_LEFT;
+    } else if (k == 38 || k == 87) {
+      // up arrow or w
+      pacman.nextDirection = DIRECTION_UP;
+    } else if (k == 39 || k == 68) {
+      // right arrow or d
+      pacman.nextDirection = DIRECTION_RIGHT;
+    } else if (k == 40 || k == 83) {
+      // bottom arrow or s
+      pacman.nextDirection = DIRECTION_BOTTOM;
+    }
+  }, 1);
+});
